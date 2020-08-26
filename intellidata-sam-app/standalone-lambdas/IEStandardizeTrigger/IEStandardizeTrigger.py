1b5b8cbc6a06d6d4da0533d11d963894bf4f3a10
Suvojit Dutta <suvojitdutta@suvojits-mbp.myfiosgateway.com>
2020-08-26 01:16:00 -0400

    new S3

--- intellidata-sam-app/standalone-lambdas/IEStandardizeTrigger/IEStandardizeTrigger.py
+++ intellidata-sam-app/standalone-lambdas/IEStandardizeTrigger/IEStandardizeTrigger.py
@@ -7,9 +7,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    glue = boto3.client(service_name='glue', region_name='us-east-1',
-	aws_access_key_id='AKIATENFQTYA3U4TNBFH' ,
-    aws_secret_access_key='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',
+    glue = boto3.client(service_name='glue',
     endpoint_url='https://glue.us-east-1.amazonaws.com')
 
     #Create a job. You must use glueetl as the name for the ETL command, as shown in the following code:
